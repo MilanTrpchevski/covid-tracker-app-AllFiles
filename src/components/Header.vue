2709e3440e2aa9702e0a791f7c8529fb763f4d2a
MilanTrpchevski <trpcevskimilan@hotmail.com>
2022-08-26 13:11:17 +0200

    app

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="text-center bg-blue-800 
+    text-white p-4 mb-10">
+      <div class="text-3xl md:text-5xl
+      font-bold mb-3"><i class="fa fa-viruses"></i>Covid-19 tracker</div>
+      <div>
+          <h4 class="text-2xl">API that i am using: <a href="https://covid19api.com/">https://covid19api.com/</a></h4>
+          <h4 class="text-2xl">You can see the raw data here: <a href="https://api.covid19api.com/">https://api.covid19api.com/</a></h4>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
